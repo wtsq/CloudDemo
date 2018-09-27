a1f5ad0b3a43a30211e09d9ffa2abee1c59b5b6b
Tylor <254255743@qq.com>
2018-09-27 20:10:08 +0800

    Initial commit

---
+++ product_service/src/main/java/net/xdclass/product_service/controller/ProductController.java
@@ -0,0 +1,64 @@
+package net.xdclass.product_service.controller;
+
+import net.xdclass.product_service.domain.Product;
+import net.xdclass.product_service.service.ProductService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/api/v1/product")
+@RefreshScope
+public class ProductController {
+
+
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${env}")
+    private String env;
+
+    @Autowired
+    private ProductService productService;
+
+    /**
+     * 获取所有商品列表
+     * @return
+     */
+    @RequestMapping("list")
+    public Object list(){
+        return productService.listProduct();
+    }
+
+
+    /**
+     * 根据id查找商品详情
+     * @param id
+     * @return
+     */
+    @RequestMapping("find")
+    public Object findById(int id){
+
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        Product product = productService.findById(id);
+
+        Product result = new Product();
+        BeanUtils.copyProperties(product,result);
+        result.setName( result.getName() + " data from port="+port +" env = "+env );
+        return result;
+    }
+
+
+
+}
