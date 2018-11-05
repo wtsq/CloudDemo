a053fff3b0f56bbfef37da1177103e51814327c7
Tylor <254255743@qq.com>
2018-11-05 20:52:39 +0800

    111

---
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/getString")
+	public String getString() {
+		return "12345789";
+	}
+}
